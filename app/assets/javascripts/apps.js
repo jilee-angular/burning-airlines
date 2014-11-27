f4a5a5e07720ff5bf2ecc86215fcf56fdd1f4cf0
Regina Rauch <reginarauch@gmail.com>
2014-11-27 15:35:59 +1100

    js mvc for flights

--- app/assets/javascripts/apps.js
+++ app/assets/javascripts/apps.js
@@ -21,3 +21,29 @@ $('#newAirplane button').on('click', function(){
   var view = new App.Views.AirplaneItemView({ model: airplane });
   $('#airplaneList ul').append(view.render().el);
 })
+
+//////////////////Add new flights/////////////////////////
+
+App.airplanes = [];
+
+App.airplanes = new App.Collections.Airplanes();
+
+App.airplanes.fetch().done(function(){
+  App.airplanes.each(function(airplane, index){
+    var view = new App.Views.AirplaneItemView({ model: airplane });
+    $('#airplaneList ul').append(view.render().el);
+  });
+
+}); // ends fetch
+
+$('#newAirplane button').on('click', function(){
+  var airplaneVal = $('#newAirplane input').val();
+  $('#newAirplane input').val('');
+
+  var airplane = new App.Models.Airplane({ body: airplaneVal });
+
+  App.airplanes.create(airplane);
+
+  var view = new App.Views.AirplaneItemView({ model: airplane });
+  $('#airplaneList ul').append(view.render().el);
+})
